68078718335949be644245a2f59dcf0fb4b983ce
missiontide <patcoronel@missiontide.com>
2022-07-05 07:00:44 -0700

    added Laravel IDE helper
    https://www.jetbrains.com/help/phpstorm/laravel.html#coding-assistance

--- config/app.php
+++ config/app.php
@@ -195,6 +195,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Laravel IDE helper
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
