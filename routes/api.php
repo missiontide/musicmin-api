c61a8051fa1d197cb52357ad1bc34db417c5284c
missiontide <patcoronel@missiontide.com>
2022-12-22 06:00:01 -0800

    trying web routes

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::get('songs', [SongController::class, 'index']);
 Route::get('songs/{song}', [SongController::class, 'show']);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/songs', [SongController::class, 'index']);
+Route::get('/songs/{song}', [SongController::class, 'show']);
+Route::post('/songs', [SongController::class, 'store']);
+Route::put('/songs/{song}', [SongController::class, 'update']);
+Route::delete('/songs/{song}', [SongController::class, 'delete']);
